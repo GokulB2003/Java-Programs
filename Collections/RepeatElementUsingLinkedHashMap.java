2b287fc9ddc2ab0d70a98218cdf7c5d86377aec7
GokulB2003 <bhorgokul8@gmail.com>
2025-12-29 18:54:12 +0530

    added new file with Map collection

---
+++ Collections/RepeatElementUsingLinkedHashMap.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class RepeatElementUsingLinkedHashMap
+{
+	public static void main(String args[])
+	{
+		int arr[]=new int[]{1,1,1,2,3,4,2,3,1,4,51,5};
+		
+		LinkedHashMap map=new LinkedHashMap();
+		
+		Set<Map.Entry>s=map.entrySet();
+		for(int i=0; i<arr.length; i++)
+		{
+			Integer count=(Integer)map.get(arr[i]);
+			if(count==null)
+			{
+				count=0;
+			}
+			++count;
+			map.put(arr[i],count);
+			
+		}
+
+		for(Map.Entry e:s)
+		{
+			if((int)e.getValue()>=2)
+			{
+				System.out.println("repeated Element is:"+e.getKey());
+			}
+		}
+	
+	}
+}
