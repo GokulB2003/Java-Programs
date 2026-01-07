5970ca4d94c365412af4265391636a8b01baa207
GokulB2003 <bhorgokul8@gmail.com>
2026-01-07 16:36:39 +0530

    added new content in File

---
+++ NestedClass/1.0.InnerClass/InnerClassConcept.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+
+	class Car
+	{
+		int speed=200;
+		class Engine
+		{
+			public void calSpeed()
+			{
+				System.out.println("the speed of the car is:"+speed);
+			}
+		}
+	}
+	
+	public class InnerClassConcept{
+
+	public static void main(String args[])
+	{
+		Car c1=new Car();
+		Car.Engine e1=c1.new Engine();
+		e1.calSpeed();
+		
+		Car c2=new Car();
+		Car.Engine e2=c2.new Engine();
+		e2.calSpeed();
+		
+	}
+	}
