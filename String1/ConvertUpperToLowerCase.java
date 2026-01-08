99676c0e9a7166f0e7a44b50513570c588d68c2b
GokulB2003 <bhorgokul8@gmail.com>
2026-01-09 00:56:29 +0530

    added new file

---
+++ String1/ConvertUpperToLowerCase.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class ConvertUpperToLowerCase
+{
+		//Without using the Inbuilt method...
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the String:");
+		String str=sc.nextLine();
+		String newString="";
+		/*for(int i=0; i<str.length(); i++)
+		{
+			if(str.charAt(i)!=' ')
+			newString+=(char)(str.charAt(i)+32);
+			else
+			newString+=" ";
+		}
+
+		*/
+		//oR
+
+		StringBuilder s=new StringBuilder();
+		for(int i=0; i<str.length(); i++)
+		{
+			char ch=str.charAt(i);
+			if(ch>='A' && ch<='Z')
+			{
+				s.append((char)(ch+32));
+			}
+			else
+			s.append(ch);
+		}
+		System.out.println("After converting the string into the Lowercase the string becomes:"+s);
+		
+		
+	}
+
+}
