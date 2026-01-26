514b023e3fcd0a24afd11eb5d5d4217ea71f5e4e
GokulB2003 <bhorgokul8@gmail.com>
2026-01-26 12:06:35 +0530

    added new file

---
+++ String1/LongestSubStringWithoutReapeatingChar.java
@@ -0,0 +1,40 @@
+/*Longest SubString Without reapeating characters...*/
+import java.util.*;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int i=0;
+        int maxl=0;
+        LinkedHashSet<Character>ans=new LinkedHashSet<>();
+        for(int j=0; j<s.length(); j++)
+        {
+                if(!ans.contains(s.charAt(j)))
+                {
+                    ans.add(s.charAt(j));
+                }
+                else
+                {
+			
+                    while(ans.contains(s.charAt(i)))
+                    {
+			
+                        ans.remove(s.charAt(i));
+                        i++;
+			
+                    }
+			ans.add(s.charAt(j));
+                }
+		  maxl=Math.max(ans.size(),maxl);
+        }
+        return maxl;
+    }
+}
+public class LongestSubStringWithoutReapeatingChar
+{
+	public static void main(String args[])
+	{
+		Solution s=new Solution();
+		String s1="abcabcbb";
+		int ans=s.lengthOfLongestSubstring(s1);
+		System.out.println(ans);
+	}
+}
