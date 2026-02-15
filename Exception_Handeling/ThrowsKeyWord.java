cd0e60869e1639c19f87274f2d7cebf1f2a3872f
GokulB2003 <bhorgokul8@gmail.com>
2026-02-15 19:57:46 +0530

    added throws

---
+++ Exception_Handeling/ThrowsKeyWord.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ThrowsKeyWord
+{
+	public static void main(String args[])throws InterruptedException
+	{
+		Thread.sleep(3000);
+		System.out.println("done");		
+	}
+}
